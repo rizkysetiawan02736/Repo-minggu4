d42a8b92a2d2ddfc86e60bae48078e29a0304244
rizkysetiawan02736 <63201220+rizkysetiawan02736@users.noreply.github.com>
2020-04-11 02:32:16 -0700

    minggu-3

---
+++ minggu3/if_else2.php
@@ -0,0 +1,8 @@
+<?php
+$user = "masaboe";
+$pass = "123"
+if ($user == "masaboe" && $pass == "123") {
+echo "Login Berhasil";
+} else {  echo "Login Gagal";
+} 
+?>
---
+++ minggu3/latihan_2_6.php
@@ -0,0 +1,10 @@
+<h2>Nomor 1 </h2>
+<?php
+
+for ($i = 1; $i <= 5; $i++) {
+    for ($k = 1; $k <= $i; $k++) {
+        echo "$i ";
+    }
+
+    echo "<br>";
+}
